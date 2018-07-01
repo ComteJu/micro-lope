b779ebda79b5c0e7511117d060ce856862df93f0
Julien Lecomte <lcmte.ju@gmail.com>
2018-07-01 19:47:39 +0200

    update cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,6 +54,12 @@ module.exports = {
       },
     },
     `gatsby-plugin-styled-components`,
-    `gatsby-plugin-netlify-cms`,
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+    'gatsby-plugin-netlify',
   ],
 }
